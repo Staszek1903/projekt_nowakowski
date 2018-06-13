315e4063821b3cefcb2d90d03e9ebb83b554d5a2
PawelGrab <pgrabczynski@poczta.onet.pl>
2018-06-13 20:48:22 +0200

    text

--- textdisplay.cpp
+++ textdisplay.cpp
@@ -7,15 +7,15 @@ TextDisplay::TextDisplay()
 
 void TextDisplay::drawCircle(int r)
 {
-    std::cout<<"text"<<std::endl;
+    std::cout<<"Drawing a circle with radius "<<r<<"."<<std::endl;
 }
 
 void TextDisplay::drawTriangle(pat::Vector2f a, pat::Vector2f b)
 {
-    std::cout<<"text"<<std::endl;
+    std::cout<<"Drawing a triangle from vectors "<<Vector2f a<<Vector2f b<<"."<<std::endl;
 }
 
 void TextDisplay::drawParralellogram(pat::Vector2f a, pat::Vector2f b)
 {
-    std::cout<<"text"<<std::endl;
+    std::cout<<"Drawing a paralellogram from vectors "<<Vector2f a<<Vector2f b<<"."<<std::endl;
 }
